8341976f11864f77afd66645bb4c1b1fefc23471
shashwat Dwivedi <itsshashwatdwivedi@gmail.com>
2024-07-13 22:07:38 +0530

    Edit some text

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import heroImagebw from '../assets/heroImagebw.jpg'
+
+export default function Card() {
+    return (
+        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center">
+            <a href="https://www.linkedin.com/in/shashwat-dwivedi-563a511aa/" target='_blank'>
+                <img className="p-8 rounded-full" src={heroImagebw} alt="product_image1" />
+            </a>
+            <div className="px-5 pb-5">
+                <a href="/">
+                    <h5 className="text-2xl font-semibold text-gray-900 dark:text-white">
+                        Hire me In Your Front-End Developer Team
+                    </h5>
+                </a>
+            </div>
+        </div>
+    );
+}
